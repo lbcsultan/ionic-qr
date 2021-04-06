fd81243131b6f3c6c425b9adf00de514addbe5fa
Byoungcheon Lee <lbcsultan@gmail.com>
2021-04-06 14:05:38 +0900

    1st

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoadingController, Platform, ToastController } from '@ionic/angular';
+import jsQR from 'jsqr';
+
+import * as forge from 'node-forge';
+const pki = forge.pki;
+
+const userCertPem =
+`-----BEGIN CERTIFICATE-----
+MIID9zCCAt+gAwIBAgIBATANBgkqhkiG9w0BAQUFADBkMQ4wDAYDVQQDEwViY2xl
+ZTELMAkGA1UEBhMCS1IxEzARBgNVBAgTCmd5ZW9uZ2dpZG8xETAPBgNVBAcTCGdv
+eWFuZ3NpMRAwDgYDVQQKEwdqb29uZ2J1MQswCQYDVQQLEwJpczAeFw0xOTEyMTAx
+MjA2MjFaFw0yMDEyMTAxMjA2MjFaMGQxDjAMBgNVBAMTBWJjbGVlMQswCQYDVQQG
+EwJLUjETMBEGA1UECBMKZ3llb25nZ2lkbzERMA8GA1UEBxMIZ295YW5nc2kxEDAO
+BgNVBAoTB2pvb25nYnUxCzAJBgNVBAsTAmlzMIIBIjANBgkqhkiG9w0BAQEFAAOC
+AQ8AMIIBCgKCAQEA7BoGIt4n6GDbsPITa6pJInh1GUIbzYOVW1y5mFp4iGsE6PFJ
+fWGyMmwYnGvLjfq58pkLyFDYAqaqvP1z+OOScINOtTuyJAYHQB0Ex/3faPK0/nFk
+EdSLln2RXtu2xyQ+/6kUiJpId1hbhmsVqd6y83KYyCIDK0zSjIul54du2YNf6gKM
+FeP8R9OnuOKZ5WV+8wTlMy/gNvBLjZrWIcRL76xrp+agu0RmLkIKG8LOuAJhVpxi
+Htt/NNRh8DN4aLPFOW4oqBXg6dxaR4ZX90zICHoVBLozS1/4N1sE7S0iXjNcCVV/
+bZV+SBp61uLDeuH4BqriI4jhh3mHzgO4i9SRkQIDAQABo4GzMIGwMAwGA1UdEwQF
+MAMBAf8wCwYDVR0PBAQDAgL0MDsGA1UdJQQ0MDIGCCsGAQUFBwMBBggrBgEFBQcD
+AgYIKwYBBQUHAwMGCCsGAQUFBwMEBggrBgEFBQcDCDARBglghkgBhvhCAQEEBAMC
+APcwJAYDVR0RBB0wG4YTaHR0cDovL2V4YW1wbGUub3JnL4cEfwAAATAdBgNVHQ4E
+FgQU4zGVncKXUxGbsnzTW/g5Jl14o6kwDQYJKoZIhvcNAQEFBQADggEBAIe+Xrr0
+OdvE5wDQccugCX5Wlia7+8XI6XBpp/X7XWp54rcOgzYJmFHU96NuYhwny88JR6wc
+9G3CLFHNxVa8INbqQyBQlz0VU4wL1giOzR8CnFov8D7t+ctdTA3SHfLW0zhIIpmB
+TI+WznnoU48EsHhNKW5rtzXSudPeqK+ef6h3GBcq4fe0QHeBzHgL/7imqxLf2Oq5
+QOknGgpkb+Aps0h8WXD7XxRi5Pk1l9nKt57Hup5BlUedf0dLRxHV4CUYDtkmikgt
+f5Qb/AfT2lb8MPINvtDjm2BLEt/qJ2+ziCYEss9CNdVrr6VS7X2Bodty2n5ONUGv
+6XrauU/YyYT42XY=
+-----END CERTIFICATE-----
+`;
+
+const userCert = pki.certificateFromPem(userCertPem);
+const userPublicKey = userCert.publicKey;
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +42,143 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+
+
+  scanActive = false;
+  scanResult = null;
+  verified = false;
+
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+
+  videoElement: any;
+  canvasElement: any;
+  canvasContext: any;
+
+  loading: HTMLIonLoadingElement;
+
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform
+  ) {
+    const isInStandaloneMode = () =>
+    'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am in an iOS PWA!');
+    }
+  }
+
+  ngAfterViewInit() {
+    this.videoElement = this.video.nativeElement;
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+  }
+
+  async startScan() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+    this.videoElement.srcObject = stream;
+    this.videoElement.setAttribute('playsinline', true);
+    this.videoElement.play();
+
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+
+  async scan() {
+    if(this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if(this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+
+      //console.log('Code: ', code);
+
+      if(code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+
+        let signedInfoObj = JSON.parse(this.scanResult);
+
+        // 전자서명 검증
+      const signature1Hex = signedInfoObj.sig;
+      const signature1 = forge.util.hexToBytes(signature1Hex);
+      const info1 = {
+        product: signedInfoObj.product,
+        email: signedInfoObj.email,
+        price: signedInfoObj.price,
+        url: signedInfoObj.url,
+        ctime: signedInfoObj.ctime
+      }
+
+      const info1String = JSON.stringify(info1);
+
+      let md1 = forge.md.sha1.create();
+      md1.update(info1String, 'utf8');
+      this.verified = userPublicKey.verify(md1.digest().bytes(), signature1);
+
+        //this.showOrToast();
+      } else {
+        if(this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  // Helper functions
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  reset() {
+    this.scanResult = null;
+  }
+
+  async showOrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,40 @@
 import { Component } from '@angular/core';
 
+import * as forge from 'node-forge';
+const pki = forge.pki;
+
+const userPrivateKeyPem =
+`-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA7BoGIt4n6GDbsPITa6pJInh1GUIbzYOVW1y5mFp4iGsE6PFJ
+fWGyMmwYnGvLjfq58pkLyFDYAqaqvP1z+OOScINOtTuyJAYHQB0Ex/3faPK0/nFk
+EdSLln2RXtu2xyQ+/6kUiJpId1hbhmsVqd6y83KYyCIDK0zSjIul54du2YNf6gKM
+FeP8R9OnuOKZ5WV+8wTlMy/gNvBLjZrWIcRL76xrp+agu0RmLkIKG8LOuAJhVpxi
+Htt/NNRh8DN4aLPFOW4oqBXg6dxaR4ZX90zICHoVBLozS1/4N1sE7S0iXjNcCVV/
+bZV+SBp61uLDeuH4BqriI4jhh3mHzgO4i9SRkQIDAQABAoIBAGUdBBG8uLShfpS6
+J60NYYLcubGWiVeribR18poX7NTnmFO2ujVzOKC1gJjE5YkpnqcR+mTQkqIYBLkp
+hphJxfZflb6yESmyVzqS8vA/foVwpCjwIN4t+5X4suBAEngYws40+DFEedJ6yb+4
+bq+wS01Nf7b+MgNCRE2ipOuSKdwGv899+IGcEgh60byN6KVvfjShq3/WuDqxEE9t
+sujdJWggGjDpNxFrB1vGYxc9rG2ajfHEfQE5awEQ7G+VC3nOybZPNL4DZgYj+VXq
+SE4ZubC7io1S/3QKyDyeHvKBjMLLRSU9D1GL2nbporJ1Q3KoDzgbFV6c9kgn3Wb1
+Pa25te0CgYEA/FFjzQtByfaWSkprakYLraJYMQooev8DKS4jWwUBWTJdomvI77H8
+yJAQmHXm5KwaMwVpJuy+/pN4Z0o42/5WMpzxqZdIla74+K5j/lSWkxDSd+Tb6D8I
+uuaT2ybUhrNraexQSjyUeNoS6xwqHmB6/pz42zpC/z6E++JH9uO7Lk8CgYEA74wN
+ph4aCoIVjLh8aRmd9HwVLxkXqqvexMS1yJl5DmauQMZjGlCF82vY7PC1WCvbq8Yn
+uPo2u4Li9z3ZT0C+bJgLjNYdrVu5VDWi5mNK/beWJPcZ9wi1XbNfgbIhH1XPKTok
+VTEMS3HqB/ZDg3TVKE/MTOP60ZxUS0gdTykfKh8CgYEAwMoDgpk2VHXIUc1ZE6Of
+qrUfw6CqW4hcRuvM8e/6HshACSloJ9WMe0awl32GxXTGs/NPbAF60hOl7O4mUdmc
+zSFj+RvxxwUhkQnWt260enxhBGDmsibvqbcihFHAE4gl65I3qvFs1IZz7BbiC8HW
+WJNiBkgXBuYjQD6elANfczsCgYEAwi0BE1rrOQFhfoQ9NOSzCW9brKXmbU6McWwS
+/tyJKIdLUO5/fuEu87bETKyCE0rZ8k/Z+zO4Eh9y43+bG4cb1OMa2brmq5PiksQ3
+nBF4xFmAt84lmf3RJnc0P+mot8HS/1RGB1iO4skgL/wLlg+qidbFgL58nlTeb3ex
+am2m+XkCgYEA9UHK3+Kv02/5ldJWgpbtE8gwysDKjKa3RtqJScGxV/6qOYBF1Lzy
+KGDh8GfeViz57vsUEsixjZKthhW9zP6uxJwA8tDke5tGvdxSvjFAC/VUXUgXRqBJ
+IkfdKbpla/tny4YpQjYWO/S8zZTxvRLNlfc0KpAWT+CLIVJipKLteDg=
+-----END RSA PRIVATE KEY-----
+`;
+
+const userPrivateKey = pki.privateKeyFromPem(userPrivateKeyPem);
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +42,44 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  public myAngularxQrCode: string = null;
+  product = "iPhone 12";
+  price = "1000000";
+  url = "http://cris.joongbu.ac.kr";
+  email = "abcde@gamil.com";
+  elementType = 'url';
+
   constructor() {}
+
+  genQR() {
+    const curr_time = new Date().getTime();
+    const info = {
+      product: this.product,
+      email: this.email,
+      price: this.price,
+      url: this.url,
+      time: curr_time
+    }
+    const infoString = JSON.stringify(info);
+
+    // 전자서명 생성
+    let md = forge.md.sha1.create();
+    md.update(infoString, 'utf8');
+    const signature = userPrivateKey.sign(md);
+    const signatureHex = forge.util.bytesToHex(signature);
+
+    const signedInfo = {
+      product: this.product,
+      email: this.email,
+      price: this.price,
+      url: this.url,
+      time: curr_time,
+      sig: signatureHex  // 서명값 추가
+    }
+    // JSON object를 string으로 변환
+    const signedInfoString = JSON.stringify(signedInfo);
+
+    this.myAngularxQrCode = signedInfoString;
+  }
 
 }
